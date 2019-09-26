0f91928e209636abb2c44a27e7d134d87e8c74d0
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-09-26 02:04:00 +0100

    All the things

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app        = require('express')()
+const bodyParser = require('body-parser').json()
+const port       = 3000
+
+const axios     = require('axios').default
+const { JSDOM } = require('jsdom')
+
+const readability = require('readability')
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => function () {
-  res.send('yay')
+app.post('/', bodyParser, (req, res) => {
+  const url = req.body.url
+
+  if (url === undefined || url === '') {
+    return res
+      .status(400)
+      .send({
+        'error': 'Send JSON, like so: {"url": "https://url/to/whatever"}'
+      })
+      .end
+  }
+
+  axios
+    .get(url)
+    .then((response) => {
+      const dom = new JSDOM(response.data)
+
+      const parsed = new readability(dom.window.document, {}).parse()
+
+      console.log(parsed)
+      return res
+        .status(200)
+        .send({
+          url: url,
+          content: parsed.content
+        })
+        .end()
+
+    })
+    .catch((error) => {
+      return res
+        .status(500)
+        .send({
+          error: 'Some weird error fetching the content',
+          details: error
+        })
+    })
 })
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Readability.js server listening on port ${port}!`))
