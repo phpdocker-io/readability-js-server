af551b1b1de7b96164e4aeb6289d383b306be9e9
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-09-26 01:18:25 +0100

    Add a few required libraries

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => function () {
+  res.send('yay')
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
