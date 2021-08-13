cba53a39fbfe1e8909f5f1fdc366f6129f7cfd78
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-08-13 14:03:52 +0100

    Ensure JSDOM runs content scripts

--- src/app.js
+++ src/app.js
@@ -44,6 +44,7 @@ app.post("/", bodyParser, (req, res) => {
 
       const dom = new JSDOM(sanitized, {
         url: url,
+        runScripts: "outside-only"
       });
 
       const parsed = new Readability(dom.window.document).parse();
